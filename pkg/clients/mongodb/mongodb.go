18b2cc36cda6c2d7c58567a31caaf909242c2c24
Kirill Smirnov <62110814+Moon1it@users.noreply.github.com>
2023-08-01 14:49:28 +0200

    Fix ConnectToMongoDB

--- pkg/clients/mongodb/mongodb.go
+++ pkg/clients/mongodb/mongodb.go
@@ -11,18 +11,17 @@ import (
 )
 
 func ConnectToMongoDB(cfg *config.Config) (*mongo.Database, error) {
-	opts := options.Client().
-		ApplyURI(cfg.MongoString).
-		SetServerAPIOptions(options.ServerAPI(options.ServerAPIVersion1))
+	clientOptions := options.Client().ApplyURI(cfg.MongoString)
 
-	client, err := mongo.Connect(context.TODO(), opts)
+	client, err := mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to MongoDB: %w", err)
 	}
 
 	// Check if the connection is successful
-	if err := client.Ping(context.TODO(), nil); err != nil {
-		_ = client.Disconnect(context.TODO()) // Attempt to disconnect on failure
+	err = client.Ping(context.Background(), nil)
+	if err != nil {
+		_ = client.Disconnect(context.Background()) // Attempt to disconnect on failure
 		return nil, fmt.Errorf("failed to ping MongoDB: %w", err)
 	}
 
